bdb587cd007b6ea82f23771a70f6d1328698adff
Pavan <gnpavan430@gmail.com>
2018-06-10 22:16:15 +0200

    Commiting the first change in the new branch

--- src/test/java/testData/Sample.java
+++ src/test/java/testData/Sample.java
@@ -51,6 +51,7 @@ public class Sample {
                 System.out.println("Discontinued date:"+ExcelWSheet.getRow(i).getCell(2));
                 System.out.println("Company:"+ExcelWSheet.getRow(i).getCell(3));
                 System.out.println("Integrated project with Git");
+                System.out.println("New branch statement");
             }
         }
 
